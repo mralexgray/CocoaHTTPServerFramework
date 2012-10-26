7bc4e9cf7aabcc71f367c538ad2db606b210b7e8
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-26 10:54:58 -0600

    Added simple tests

---
+++ CocoaHTTPServerTests/CocoaHTTPServerTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CocoaHTTPServerTests : SenTestCase
+
+@end
---
+++ CocoaHTTPServerTests/CocoaHTTPServerTests.m
@@ -0,0 +1,26 @@
+#import "CocoaHTTPServerTests.h"\
+
+#import <CocoaHTTPServer/HTTPServer.h>
+
+@implementation CocoaHTTPServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    HTTPServer * m = [HTTPServer new];
+}
+
+@end
