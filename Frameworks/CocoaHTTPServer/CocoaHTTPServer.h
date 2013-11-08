cde48a635fff0011aaee2450262dd22ebc1e30ca
Alex Gray <alex@mrgray.com>
2013-11-07 22:38:34 -0500

    corrected linker flags, runpaths, copy phases, all the way down and added an umbrella header

---
+++ Frameworks/CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,31 @@
+//
+//  CocoaHTTPServer.h>
+//  CocoaHTTPServer
+//
+//  Created by Alex Gray on 11/7/13.
+//  Copyright (c) 2013 ASE Group. All rights reserved.
+//
+
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
+#import <CocoaHTTPServer/DDData.h>
+#import <CocoaHTTPServer/DDNumber.h>
+#import <CocoaHTTPServer/DDRange.h>
+#import <CocoaHTTPServer/DAVConnection.h>
+#import <CocoaHTTPServer/DAVResponse.h>
+#import <CocoaHTTPServer/DELETEResponse.h>
+#import <CocoaHTTPServer/PUTResponse.h>
+#import <CocoaHTTPServer/MultipartFormDataParser.h>
+#import <CocoaHTTPServer/MultipartMessageHeader.h>
+#import <CocoaHTTPServer/MultipartMessageHeaderField.h>
+#import <CocoaHTTPServer/HTTPAuthenticationRequest.h>
+#import <CocoaHTTPServer/HTTPConnection.h>
+#import <CocoaHTTPServer/HTTPMessage.h>
+#import <CocoaHTTPServer/HTTPResponse.h>
+#import <CocoaHTTPServer/HTTPServer.h>
+#import <CocoaHTTPServer/HTTPAsyncFileResponse.h>
+#import <CocoaHTTPServer/HTTPDataResponse.h>
+#import <CocoaHTTPServer/HTTPDynamicFileResponse.h>
+#import <CocoaHTTPServer/HTTPFileResponse.h>
+#import <CocoaHTTPServer/HTTPRedirectResponse.h>
+#import <CocoaHTTPServer/WebSocket.h>
